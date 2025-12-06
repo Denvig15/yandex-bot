4c956d17dd3046f19984f49e6f31b4c67edb82ea
Denvig15 <96308701+Denvig15@users.noreply.github.com>
2025-12-06 23:58:01 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,39 @@
+
+import telebot
+
+BOT_TOKEN = '7973616652:AAFmRe7VUGtJ3ARKdB5str1zi12fNtl4grM'
+YOUR_CHAT_ID = 1322005220  # твой Telegram ID
+
+bot = telebot.TeleBot(BOT_TOKEN)
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(
+        message.chat.id,
+        "👋 Привет! Напиши, пожалуйста, свой @username (или как с тобой связаться)."
+    )
+    # говорим боту: следующий ответ этого юзера обработать в функции get_username
+    bot.register_next_step_handler(message, get_username)
+
+
+def get_username(message):
+    user_text = message.text  # что написал пользователь
+
+    # отправляем инфу тебе
+    bot.send_message(
+        YOUR_CHAT_ID,
+        f"Новый пользователь:\n"
+        f"chat_id: {message.chat.id}\n"
+        f"user_id: {message.from_user.id}\n"
+        f"username из ответа: {user_text}"
+    )
+
+    # отвечаем пользователю
+    bot.send_message(
+        message.chat.id,
+        "Спасибо! Данные переданы продавцу, скоро свяжемся 🙂"
+    )
+
+
+bot.polling()
